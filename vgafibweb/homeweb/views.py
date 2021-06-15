9634b9f6f25f1da0e95fa61cfda3785ded0922af
OODDer <35957751+OODDer@users.noreply.github.com>
2021-06-15 13:32:27 +0200

    desactivamos home

--- vgafibweb/homeweb/views.py
+++ vgafibweb/homeweb/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 # Create your views here.
 
 
+# def index(request):
+#     return render(request, "home.html")
+
+
 def index(request):
-    return render(request, "home.html")
+    return redirect('cursos_index')
--- vgafibweb/vgafibweb/urls.py
+++ vgafibweb/vgafibweb/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('cursos/', include('cursos.urls')),
-    path('home/', include('homeweb.urls')),
-    path('admin/', admin.site.urls),
+    path('', include('homeweb.urls')),
+    path('admin/', admin.site.urls)
 
 ]
